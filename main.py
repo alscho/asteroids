ecb1ee90cf5a2f398e187d3dd2a5c9471c7fe94c
alscho <alexander2791@hotmail.com>
2024-11-28 12:10:21 +0100

    added player

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 import pygame
 
+### import own files
+from player import *
 from constants import *
 
 def main():
+
+    ### initiate pygame and open screen
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    ### opening game clock
     clock = pygame.time.Clock()
     dt = 0
 
+    ### instantiate Player
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+
+    ### game loop
     while(1):
+
+        ### closing windows
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+
+        ### blacken screen and draw player
         screen.fill(color="#000000")
-        pygame.display.flip()
+        player.draw(screen)
+        pygame.display.flip()        
 
-
-
+        ### limit FPS to 60 and get actual delta time
         dt = clock.tick(60)/1000
 
 
