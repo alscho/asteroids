f803ac00126eaea7f44a02aacbd9f5dacf369bcb
alscho <alexander2791@hotmail.com>
2024-11-28 11:52:35 +0100

    added clock and fixed max FPS to 60

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while(1):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -13,6 +16,11 @@ def main():
             
         screen.fill(color="#000000")
         pygame.display.flip()
+
+
+
+        dt = clock.tick(60)/1000
+
 
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
